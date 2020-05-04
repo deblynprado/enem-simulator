fd54a9e28ed79d2b2180017cc47d5416cd1cd486
Deblyn Prado <deblynprado@hotmail.com>
2020-05-04 10:18:53 -0300

    Including ACF PRO

--- enem-simulator.php
+++ enem-simulator.php
@@ -2,23 +2,40 @@
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 /**
- * Enem Simulator
- *
- * @package           EnemSimulator
- * @author            Deblyn Prado, Walfrido Oliveira
- * @copyright         2019 Deblyn Prado, Walfrido Oliveira
- * @license           GPL-2.0-or-later
- *
- * @wordpress-plugin
- * Plugin Name:       Enem Simulator
- * Plugin URI:        https://github.com/deblynprado/enem-simulator
- * Description:       Allows your users to generate random tests and check their knowledge.
- * Version:           1.0.0
- * Requires at least: 5.0
- * Requires PHP:      7.2
- * Author:            Deblyn Prado, Walfrido Oliveira
- * Author URI:        https://example.com
- * Text Domain:       plugin-slug
- * License:           GPL v2 or later
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- */
+* Enem Simulator
+*
+* @package           EnemSimulator
+* @author            Deblyn Prado, Walfrido Oliveira
+* @copyright         2019 Deblyn Prado, Walfrido Oliveira
+* @license           GPL-2.0-or-later
+*
+* @wordpress-plugin
+* Plugin Name:       Enem Simulator
+* Plugin URI:        https://github.com/deblynprado/enem-simulator
+* Description:       Allows your users to generate random tests and check their knowledge.
+* Version:           1.0.0
+* Requires at least: 5.0
+* Requires PHP:      7.2
+* Author:            Deblyn Prado, Walfrido Oliveira
+* Author URI:        https://example.com
+* Text Domain:       plugin-slug
+* License:           GPL v2 or later
+* License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+$dir = plugin_dir_path( __FILE__ );
+
+define( 'ACF_PATH', $dir . '/includes/acf/' );
+define( 'ACF_URL', $dir . '/includes/acf/' );
+
+include_once( ACF_PATH . 'acf.php' );
+
+add_filter('acf/settings/url', 'acf_settings_url');
+function acf_settings_url( $url ) {
+  return ACF_URL;
+}
+
+#add_filter('acf/settings/show_admin', 'acf_settings_show_admin');
+function acf_settings_show_admin( $show_admin ) {
+  return false;
+}
