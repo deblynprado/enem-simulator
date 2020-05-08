10bbdaf425ff5f898214602d14dcfe3af695d85b
Deblyn Prado <deblynprado@hotmail.com>
2020-05-08 09:52:37 -0300

    Fixing typo

--- acf-options.php
+++ acf-options.php
@@ -3,8 +3,8 @@
 if( function_exists('acf_add_options_page') ) {
 	
 	acf_add_options_page(array(
-		'page_title' 	=> 'Settingns',
-		'menu_title'	=> 'Settingns',
+		'page_title' 	=> 'Settings',
+		'menu_title'	=> 'Settings',
 		'menu_slug' 	=> 'enem-simulator-settingns',
 		'parent_slug'	=> 'enem-simulator-main',
 		'redirect'		=> false
