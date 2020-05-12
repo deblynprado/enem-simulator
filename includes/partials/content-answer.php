441240c9ea54102a285e8a0f43d34f2ab7f56727
walfrido-oliveira <walfrido_15@hotmail.com>
2020-05-12 11:10:42 -0300

    adding radio in answer options

--- includes/partials/content-answer.php
+++ includes/partials/content-answer.php
@@ -29,12 +29,15 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 	<h4><?php the_title() ?></h4>
     <p> <?php the_content() ?> </p>
     <div>
-        <?php if( $fields ): ?>
-            <ul>
-            <?php foreach( $fields as $name => $value ): ?>
-                <li><?php echo $value['text_answer'] ?></li>
-            <?php endforeach; ?>
-            </ul>
+        <?php if( $fields ): $name = substr( md5( serialize( $fields ) ), 0, 8 ); ?>
+            <div>
+              <?php foreach( $fields as $value ): $id = substr( md5( serialize( $value ) ), 0, 8 ); ?>
+                <div class="custom-control custom-radio">
+                  <input type="radio" class="custom-control-input" id="<?php echo $id ?>" name="<?php echo $name ?>">
+                  <label class="custom-control-label" for="<?php echo $id ?>"><?php echo $value['text_answer'] ?></label>
+                </div>
+              <?php endforeach; ?>
+            </div>
         <?php endif; ?>
 	</div>
 </div>
