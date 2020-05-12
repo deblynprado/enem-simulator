bce851b1a06da937448fb47a4f3387936dfbd8d8
walfrido-oliveira <walfrido_15@hotmail.com>
2020-05-12 10:11:17 -0300

    adding function to get posts by AJAX

--- enem-simulator.php
+++ enem-simulator.php
@@ -32,6 +32,12 @@ function enqueue_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_styles' );
 
+function enqueue_scripts() {
+  wp_enqueue_script( 'jQuery' );
+  wp_enqueue_script( 'enem-simulator', plugins_url( 'includes/assets/bootstrap/js/enem-simulator.js', __FILE__ ), null, null, true );
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_scripts' );
+
 function enem_simulator_shortcode( $atts ) {
 
   include ( 'includes/partials/content-initial.php' );
@@ -66,7 +72,7 @@ function enem_simulator_shortcode( $atts ) {
 
       $fields = get_field( 'text_options', get_the_ID() ); 
 
-      include ( 'includes/partials/content-answer.php' );
+      //include ( 'includes/partials/content-answer.php' );
     }
   }
 
@@ -74,3 +80,40 @@ function enem_simulator_shortcode( $atts ) {
     
 }
 add_shortcode( 'enem-simulator', 'enem_simulator_shortcode' );
+
+function enem_simulator_get_question_category_callback() {
+
+  if ( isset($_POST['category'] )) $categories[] = $_POST['category'];
+
+  $args = array(
+    'post_type' => 'question',
+    'tax_query' => array(
+        array(
+          'taxonomy' => 'question_category',
+          'field' => 'id',
+          'terms'    => $categories,
+        ),
+    ),
+  );
+
+  $questions = new WP_Query( $args );
+
+  if ( $questions->have_posts( ) ) {
+
+    while ( $questions->have_posts() ) {  
+
+      $questions->the_post();
+
+      $fields = get_field( 'text_options', get_the_ID() ); 
+
+      include ( 'includes/partials/content-answer.php' );
+    }
+  }
+
+  wp_reset_postdata();
+
+   wp_die();
+}
+
+add_action( 'wp_ajax_enem_simulator_get_question_category', 'enem_simulator_get_question_category_callback' );
+add_action( 'wp_ajax_nopriv_enem_simulator_get_question_category', 'enem_simulator_get_question_category_callback' );
