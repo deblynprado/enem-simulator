71cd2b55e335715c8a06c560c07bef1dad64a761
Deblyn Prado <deblynprado@hotmail.com>
2020-05-13 11:22:26 -0300

    Slipt part of content simulator + fixing strings

--- includes/partials/content-simulator.php
+++ includes/partials/content-simulator.php
@@ -1,53 +1,46 @@
 <?php defined( 'ABSPATH' ) or die( 'No script kiddies please!' ); ?>
 
-<div class="contaner">
-  <div class="row content-simulator">
-    <div class="col-12">
-        <h2><?php echo __( 'Enem Simulator', 'enem-simulator' ) ?></h2>
-        <p> <?php the_field( 'initial_message', 'option' ) ?> </p>
-        <p> <?php echo __('Choose a desired category to start') ?> </p>
+<div class="container">
+  <div class="row simulator-header">
+    <div class="col">
+      <h2><?php echo __( 'Enem Simulator', 'enem-simulator' ) ?></h2>
+      <p> <?php the_field( 'initial_message', 'option' ) ?> </p>
+      <p> <?php echo __('Choose a desired category to start') ?> </p>
     </div>
-    <div class="col-12">
-      <select class="form-control form-control-lg" name="question_category" id="question_category">
-        <?php
+  </div><!-- /.simulator-header -->
+    
+  <div class="row simulator-category-options">
+    <?php if( get_field( 'question_categories', 'option' ) ) : ?>
+      <div class="col">  
+        <select class="form-control form-control-lg" name="question_category" id="question_category">
+          <?php
           $categories = get_field( 'question_categories', 'option' ); 
           foreach ($categories as $value) : ?>
-            <option value="<?php echo $value[ 'question_category' ]->term_id ?>"><?php echo $value[ 'question_category' ]->name ?></option>
+          <option value="<?php echo $value[ 'question_category' ]->term_id ?>"><?php echo $value[ 'question_category' ]->name ?></option>
           <?php endforeach; ?>
-      </select>
-    </div>
-    <div class="col-12">
+        </select>
+      </div><!-- /.col -->
+    <?php endif; ?>
+
+    <div class="col">
       <button class="btn btn-primary" id="start-simulator"><?php echo __( 'Start Simulator', 'enem-simulator' ) ?></button>
-    </div>
+    </div><!-- /.col -->
   </div>
-  <div class="row">
-    <form name="content-question-form" id="content-question-form" enctype="multipart/form-data" method="post">
-      <div class="col-12 content-question">
-        
-      </div>
-    </form>
-    <div class="col-12 nav" style="display: none">
-      <nav>
-        <ul class="pagination">
-          <li class="page-item">
-            <a class="page-link" href="#" aria-label="Previous" id="previous-question">
-              <span aria-hidden="true">&laquo;</span>
-              <span class="sr-only">Previous</span>
-            </a>
-          </li>
-          <li class="page-item">
-            <a class="page-link" href="#" aria-label="Next" id="next-question">
-              <span aria-hidden="true">&raquo;</span>
-              <span class="sr-only">Next</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
+</div><!-- /.simulator-category-options -->
+
+<div class="row">
+  <form name="content-question-form" id="content-question-form" enctype="multipart/form-data" method="post">
+    <div class="col-12 content-question">
+      
     </div>
+  </form>
+  <!-- pagination -->
+  <?php include ( 'simulator-pagination.php' ); ?>
+  <!-- /pagination -->
+</div>
+<div class="row end-simulator" style="display: none">
+  <div class="col-12">
+    <button  type="submit" class="btn btn-primary" id="end-simulator"><?php echo __( 'End Simulator', 'enem-simulator' ) ?></button>
   </div>
-  <div class="row end-simulator" style="display: none">
-    <div class="col-12">
-      <button  type="submit" class="btn btn-primary" id="end-simulator"><?php echo __( 'End Simulator', 'enem-simulator' ) ?></button>
-    </div>
-  </div>
+</div>
 </div>
---
+++ includes/partials/simulator-pagination.php
@@ -0,0 +1,18 @@
+<div class="col-12 nav" style="display: none">
+    <nav>
+      <ul class="pagination">
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Previous" id="previous-question">
+            <span aria-hidden="true">&laquo;</span>
+            <span class="sr-only"><?php _ex( 'Previous', 'Navigation to Previous Question', 'enem-simulator') ?></span>
+          </a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Next" id="next-question">
+            <span aria-hidden="true">&raquo;</span>
+            <span class="sr-only"><?php _ex( 'Next', 'Navigation to Next Question', 'enem-simulator') ?></span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </div>
