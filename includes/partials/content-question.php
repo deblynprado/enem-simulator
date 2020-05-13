854f1b4a33fb19323475689720f88372f49c374c
walfrido-oliveira <walfrido_15@hotmail.com>
2020-05-13 17:15:11 -0300

    changing input type to checkbox

--- includes/partials/content-question.php
+++ includes/partials/content-question.php
@@ -8,8 +8,8 @@
     <?php if( $fields ): $name = substr( md5( serialize( $fields ) ), 0, 8 ); ?>
     <div>
       <?php foreach( $fields as $value ): $id = substr( md5( serialize( $value ) ), 0, 8 ); ?>
-      <div class="custom-control custom-radio">
-        <input type="radio" class="custom-control-input" id="<?php echo $id ?>" name="<?php echo $name ?>">
+      <div class="custom-control custom-checkbox">
+        <input type="checkbox" class="custom-control-input" id="<?php echo $id ?>" name="<?php echo $name ?>">
         <label class="custom-control-label" for="<?php echo $id ?>"><?php echo $value['text_answer'] ?></label>
       </div>
       <?php endforeach; ?>
