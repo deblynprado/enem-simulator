7cbca60769e4bc8af11558cb5848ad3d99774952
walfrido-oliveira <walfrido_15@hotmail.com>
2020-05-13 17:57:17 -0300

    adding revise button

--- includes/partials/simulator-pagination.php
+++ includes/partials/simulator-pagination.php
@@ -3,13 +3,19 @@
       <ul class="pagination">
         <li class="page-item">
           <a class="page-link" href="#" aria-label="Previous" id="previous-question">
-            <span aria-hidden="true">&laquo;</span>
+            <span aria-hidden="true"><i class="fa fa-chevron-left"></i> <?php echo __('Previous', 'enem-simulator'); ?></span>
             <span class="sr-only"><?php _ex( 'Previous', 'Navigation to Previous Question', 'enem-simulator') ?></span>
           </a>
         </li>
         <li class="page-item">
           <a class="page-link" href="#" aria-label="Next" id="next-question">
-            <span aria-hidden="true">&raquo;</span>
+            <span aria-hidden="true"><?php echo __('Next', 'enem-simulator'); ?> <i class="fa fa-chevron-right"></i></i></span>
+            <span class="sr-only"><?php _ex( 'Next', 'Navigation to Next Question', 'enem-simulator') ?></span>
+          </a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Next" id="next-question">
+            <span aria-hidden="true"><?php echo __('Revise', 'enem-simulator'); ?> <i class="fa fa-check"></i></i></span>
             <span class="sr-only"><?php _ex( 'Next', 'Navigation to Next Question', 'enem-simulator') ?></span>
           </a>
         </li>
