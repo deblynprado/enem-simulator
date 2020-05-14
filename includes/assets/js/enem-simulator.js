bede2596e560522e1e2eeb1ac2c7263d584a446e
walfrido-oliveira <walfrido_15@hotmail.com>
2020-05-14 11:00:56 -0300

    adding progress configuration + single checkbox checked

--- includes/assets/js/enem-simulator.js
+++ includes/assets/js/enem-simulator.js
@@ -49,6 +49,7 @@ jQuery(document).ready(function( $ ) {
       $('.question:visible').hide('slow');
       $('.question:first').show('slow');
     }
+    setProgressbar();
     return false;
 
   });
@@ -62,9 +63,52 @@ jQuery(document).ready(function( $ ) {
       $('.question:visible').hide('slow');
       $('.question:last').show('slow');
     }
+    setProgressbar();
     return false;
 
   });
 
+  $(document).on('click', 'input[type=checkbox]', function(e) {
+    let $box = $(this);
+
+    if ($box.is(':checked')) {
+      var group = 'input:checkbox[name=' + $box.attr("name") + ']';
+      $(group).prop('checked', false);
+      $box.prop('checked', true);
+    } else {
+      $box.prop('checked', false);
+    }
+  });
+
+  function setProgressbar() {
+    let questions = $('.question');
+    let progressBar = $(".progress-bar");
+    let count = questions.length;
+    let index =  $('.question:visible').index();
+
+    let percent = 100 / count * (index + 1);
+
+    progressBar.attr('aria-valuenow', percent);
+    progressBar.html(percent);
+
+    let delay = 500;
+    progressBar.each(function(i) {
+      $(this).delay(delay * i).animate({
+        width: $(this).attr('aria-valuenow') + '%'
+      }, delay);
+
+      $(this).prop('Counter', 0).animate({
+        Counter: $(this).text()
+      }, {
+        duration: delay,
+        // easing: 'swing',
+        step: function(now) {
+          $(this).text(Math.ceil(now) + '%');
+        }
+      });
+    });
+  }
+
 });
+
 
