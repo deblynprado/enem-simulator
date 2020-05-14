211f51def9ef11cf2bbf2dca27aec1cc9337a546
walfrido-oliveira <walfrido_15@hotmail.com>
2020-05-14 11:02:14 -0300

    set initial value to 0

--- includes/partials/simulator-progress.php
+++ includes/partials/simulator-progress.php
@@ -1,5 +1,5 @@
 <div class="col-10">
   <div class="progress question-progress mt-4" style="display: none">
-      <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+      <div class="progress-bar" role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">0%</div>
   </div>
 </div>
