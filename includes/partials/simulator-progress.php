ef8858021baf55a1c1ca1a49486d873e157c289b
walfrido-oliveira <walfrido_15@hotmail.com>
2020-05-14 09:38:40 -0300

    adding progress-bar

---
+++ includes/partials/simulator-progress.php
@@ -0,0 +1,5 @@
+<div class="col-10">
+  <div class="progress question-progress mt-4" style="display: none">
+      <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+  </div>
+</div>
