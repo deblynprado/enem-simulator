92112d0c1fb4142e1e8cabe15224af9a869ecc05
walfrido-oliveira <walfrido_15@hotmail.com>
2020-05-14 15:38:39 -0300

    adding initial value to timer

--- includes/partials/simulator-timer.php
+++ includes/partials/simulator-timer.php
@@ -1,4 +1,6 @@
-<div class="col-10">
-  <div class="timer" style="display: none">
+<div class="col-10 mt-4">
+  <div class="timer float-right" style="display: none">
+    <p>00:00:00</p>
+    <?php echo __('Time','enem-simulator') ?>
   </div>
 </div>
