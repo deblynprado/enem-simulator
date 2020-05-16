0b1827addb3aa3239b008c36f8dee76f9fbfbb90
walfrido-oliveira <walfrido_15@hotmail.com>
2020-05-16 18:24:03 -0300

    removed duplicated styles

--- includes/partials/simulator-timer.php
+++ includes/partials/simulator-timer.php
@@ -3,6 +3,6 @@
 <div class="col-10 mt-4">
   <div class="timer float-right" style="display: none">
     <p>00:00:00</p>
-    <?php echo __('Time','enem-simulator') ?>
+    <span><?php echo __('Time','enem-simulator') ?></span>
   </div>
 </div>
