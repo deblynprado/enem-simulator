e3791adc118f7605cb5f5d3a31cac7ffad83b7b8
walfrido-oliveira <walfrido_15@hotmail.com>
2020-05-16 14:39:51 -0300

    changing div display none

--- includes/partials/simulator-progress.php
+++ includes/partials/simulator-progress.php
@@ -1,7 +1,7 @@
 <?php defined( 'ABSPATH' ) or die( 'No script kiddies please!' ); ?>
 
-<div class="col-10 simulator-progress">
-  <div class="progress mt-4" style="display: none">
+<div class="col-10 simulator-progress" style="display: none">
+  <div class="progress mt-4">
       <div class="progress-bar" role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">0%</div>
   </div>
 </div>
