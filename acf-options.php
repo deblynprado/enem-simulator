84384220f915a783932d3d8e5af81082a32116f7
walfrido-oliveira <walfrido_15@hotmail.com>
2020-05-20 17:04:17 -0300

    updating labels to pt-br

--- acf-options.php
+++ acf-options.php
@@ -3,8 +3,8 @@
 if( function_exists('acf_add_options_page') ) {
   
 	acf_add_options_page(array(
-		'page_title' 	=> __( 'Settings', 'enem-simulator' ) ,
-		'menu_title'	=> __( 'Settings', 'enem-simulator' ),
+		'page_title' 	=> __( 'Configurações', 'enem-simulator' ) ,
+		'menu_title'	=> __( 'Configurações', 'enem-simulator' ),
 		'menu_slug' 	=> 'enem-simulator-settingns',
 		'parent_slug'	=> 'enem-simulator-main',
 		'redirect'		=> false
