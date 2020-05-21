2de4a4c0afecba4a6f3c3c5504880bce2947eb89
walfrido-oliveira <walfrido_15@hotmail.com>
2020-05-21 15:33:06 -0300

    Change name page

--- acf-options.php
+++ acf-options.php
@@ -3,7 +3,7 @@
 if( function_exists('acf_add_options_page') ) {
   
 	acf_add_options_page(array(
-		'page_title' 	=> __( 'Configurações', 'enem-simulator' ) ,
+		'page_title' 	=> __( 'Enem Simulator', 'enem-simulator' ) ,
 		'menu_title'	=> __( 'Configurações', 'enem-simulator' ),
 		'menu_slug' 	=> 'enem-simulator-settingns',
 		'parent_slug'	=> 'enem-simulator-main',
