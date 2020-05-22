0dffd03f65b4d6e6d7d06a0b115c8254bb052d0e
walfrido-oliveira <walfrido_15@hotmail.com>
2020-05-22 13:48:47 -0300

    adding class name

--- includes/partials/simulator-footer.php
+++ includes/partials/simulator-footer.php
@@ -2,7 +2,7 @@
 
 <div class="col-10 mt-5 simulator-footer" style="display: none;">
   <div class="btn-group mr-4">
-    <button type="button" class="btn btn-secondary mr-2"><i class="fa fa-expand"></i></button>
+    <button type="button" class="btn btn-secondary mr-2 fullscreem-simulator"><i class="fa fa-expand"></i></button>
     <button type="button" class="btn btn-secondary ml-2 simulator-category-list"><i class="fa fa-list"></i></button>
   </div>
   <div class="btn-group mr-4">
