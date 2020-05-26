2e2d9286b3e4fc5740ca73c5e928ad4dcd21355a
walfrido-oliveira <walfrido_15@hotmail.com>
2020-05-26 15:51:25 -0300

    translating option page

--- acf-options.php
+++ acf-options.php
@@ -3,11 +3,13 @@
 if( function_exists('acf_add_options_page') ) {
   
 	acf_add_options_page(array(
-		'page_title' 	=> __( 'Enem Simulator', 'enem-simulator' ) ,
+		'page_title' 	=> __( 'Simulado do Enem', 'enem-simulator' ) ,
 		'menu_title'	=> __( 'Configurações', 'enem-simulator' ),
 		'menu_slug' 	=> 'enem-simulator-settingns',
 		'parent_slug'	=> 'enem-simulator-main',
-		'redirect'		=> false
+    'redirect'		=> false,
+    'update_button' => __('Salvar', 'acf'),
+    'updated_message' => __("Configurações salvas", 'acf'),
 	));
 	
 }
