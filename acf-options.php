06d9a4f94e8b664c5f356c79d11ce7d41d3993f9
walfrido-oliveira <walfrido_15@hotmail.com>
2020-05-29 10:30:43 -0300

    adding option page acf to import and export questions

--- acf-options.php
+++ acf-options.php
@@ -10,6 +10,16 @@ if( function_exists('acf_add_options_page') ) {
     'redirect'		=> false,
     'update_button' => __('Salvar', 'acf'),
     'updated_message' => __("Configurações salvas", 'acf'),
+  ));
+  
+  acf_add_options_page(array(
+		'page_title' 	=> __( '', 'enem-simulator' ) ,
+		'menu_title'	=> __( 'Importar/Exportar', 'enem-simulator' ),
+		'menu_slug' 	=> 'enem-simulator-import-export',
+		'parent_slug'	=> 'enem-simulator-main',
+    'redirect'		=> false,
+    'update_button' => __('', 'acf'),
+    'updated_message' => __("Configurações salvas", 'acf'),
 	));
 	
 }
