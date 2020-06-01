1618a0406dd8f566a22281e6b141cf7fd97c9ed1
walfrido-oliveira <walfrido_15@hotmail.com>
2020-06-01 15:40:46 -0300

    removing unless variable

--- enem-simulator-functions.php
+++ enem-simulator-functions.php
@@ -5,40 +5,44 @@ function enem_simulator_import_callback() {
     $file = $_FILES[ 'file' ][ 'tmp_name' ];
   }
   $row = 0;
-  if( $file ) {
-    if( ( $handle = fopen( $file, 'r' )) !== FALSE ) {
-      while( ( $data = fgetcsv( $handle, 1000, ";" )) !== FALSE ) {
-        if( $row !== 0 ) {
-          $num = count( $data );
-          $question = array(
-            'post_title' =>  $data[0],
-            'post_content' => $data[1],
-            'post_type' => 'question',
-            'tax_input' => array(
-              'question_category' => array( 6 ),
-            ),
-            'post_status' => 'publish',
-            'post_author' => 1,
-            'meta_input' => $meta_input,
-          );
-          $the_post_id = wp_insert_post( $question, $wp_error );
-          var_dump($the_post_id);
-          update_field( 'field_5eb02790f5dc1', 'Text Options', $the_post_id ); //type_of_anser
-          update_field( 'field_5ecd5537b4bdc', $data[9] , $the_post_id ); //weight_ansewer
-          update_field( 'field_5eb02ac918c41', 'active', $the_post_id ); //active_question
-          for ($c=0; $c < 5; $c++) {
-            $correctAnswer = [];
-            if( $data[8] == ( $c + 1 ) ) 
-              $correctAnswer = array( "correct" );
-            add_row( 'field_5eb028ccf5dc5', array(
-              'field_5eb028e1f5dc6' => $data[ $c + 3 ],
-              'field_5eb02909f5dc7' => $correctAnswer,
-            ), $the_post_id); //options;
+  try {
+    if( $file ) {
+      if( ( $handle = fopen( $file, 'r' )) !== FALSE ) {
+        while( ( $data = fgetcsv( $handle, 0, ";" )) !== FALSE ) {
+          if( $row !== 0 ) {
+            $num = count( $data );
+            $question = array(
+              'post_title' =>  $data[0],
+              'post_content' => $data[1],
+              'post_type' => 'question',
+              'tax_input' => array(
+                'question_category' => array( enem_simulator_get_question_category_ID( $data[2] ) ),
+              ),
+              'post_status' => 'publish',
+              'post_author' => 1,
+            );
+            $the_post_id = wp_insert_post( $question, $wp_error );
+            update_field( 'field_5eb02790f5dc1', 'Text Options', $the_post_id ); //type_of_anser
+            update_field( 'field_5ecd5537b4bdc', $data[9] , $the_post_id ); //weight_ansewer
+            update_field( 'field_5eb02ac918c41', 'active', $the_post_id ); //active_question
+            for ($c=0; $c < 5; $c++) {
+              $correctAnswer = [];
+              if( $data[8] == ( $c + 1 ) ) 
+                $correctAnswer = array( "correct" );
+              add_row( 'field_5eb028ccf5dc5', array(
+                'field_5eb028e1f5dc6' => $data[ $c + 3 ],
+                'field_5eb02909f5dc7' => $correctAnswer,
+              ), $the_post_id); //options;
+            }
           }
+          $row++;
         }
-        $row++;
       }
     }
+  } catch (\Throwable $th) {
+    wp_send_json( array(
+      'message' => $th->getMessage
+    ));
   }
   $return = array(
     'message' => __('Posts imported successfuly!', 'enem-simulator' )
