c7eb0d36cdc80d942e3fdfd3799ba080fa3dd418
walfrido-oliveira <walfrido_15@hotmail.com>
2020-06-01 16:45:50 -0300

    changing titles

--- acf-options.php
+++ acf-options.php
@@ -3,7 +3,7 @@
 if( function_exists('acf_add_options_page') ) {
   
 	acf_add_options_page(array(
-		'page_title' 	=> __( '', 'enem-simulator' ) ,
+		'page_title' 	=> __( 'Configurações', 'enem-simulator' ) ,
 		'menu_title'	=> __( 'Configurações', 'enem-simulator' ),
 		'menu_slug' 	=> 'enem-simulator-settingns',
 		'parent_slug'	=> 'enem-simulator-main',
@@ -13,7 +13,7 @@ if( function_exists('acf_add_options_page') ) {
   ));
   
   acf_add_options_page(array(
-		'page_title' 	=> __( '', 'enem-simulator' ) ,
+		'page_title' 	=> __( 'Importar/Exportar', 'enem-simulator' ) ,
 		'menu_title'	=> __( 'Importar/Exportar', 'enem-simulator' ),
 		'menu_slug' 	=> 'enem-simulator-import-export',
 		'parent_slug'	=> 'enem-simulator-main',
