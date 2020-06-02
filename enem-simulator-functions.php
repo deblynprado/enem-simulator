308c35db4d6ff8e0640ba799fae1bccb2105d7a3
walfrido-oliveira <walfrido_15@hotmail.com>
2020-06-01 21:23:23 -0300

    adding import settings

--- enem-simulator-functions.php
+++ enem-simulator-functions.php
@@ -1,9 +1,8 @@
 <?php
 
 function enem_simulator_import_questions_callback() {
-  if( isset( $_FILES )) {
+  if( isset( $_FILES )) 
     $file = $_FILES[ 'file' ][ 'tmp_name' ];
-  }
   $row = 0;
   try {
     if( $file ) {
@@ -11,28 +10,31 @@ function enem_simulator_import_questions_callback() {
         while( ( $data = fgetcsv( $handle, 0, ";" )) !== FALSE ) {
           if( $row !== 0 ) {
             $num = count( $data );
-            $question = array(
-              'post_title' =>  $data[0],
-              'post_content' => $data[1],
-              'post_type' => 'question',
-              'tax_input' => array(
-                'question_category' => array( enem_simulator_get_question_category_ID( $data[2] ) ),
-              ),
-              'post_status' => 'publish',
-              'post_author' => 1,
-            );
-            $the_post_id = wp_insert_post( $question, $wp_error );
-            update_field( 'field_5eb02790f5dc1', 'Text Options', $the_post_id ); //type_of_anser
-            update_field( 'field_5ecd5537b4bdc', $data[9] , $the_post_id ); //weight_ansewer
-            update_field( 'field_5eb02ac918c41', 'active', $the_post_id ); //active_question
-            for ($c=0; $c < 5; $c++) {
-              $correctAnswer = [];
-              if( $data[8] == ( $c + 1 ) ) 
-                $correctAnswer = array( "correct" );
-              add_row( 'field_5eb028ccf5dc5', array(
-                'field_5eb028e1f5dc6' => $data[ $c + 3 ],
-                'field_5eb02909f5dc7' => $correctAnswer,
-              ), $the_post_id); //options;
+            $categoryID = enem_simulator_get_question_category_ID( $data[2] );
+            if( $categoryID ) {
+              $question = array(
+                'post_title' =>  $data[0],
+                'post_content' => $data[1],
+                'post_type' => 'question',
+                'tax_input' => array(
+                  'question_category' => array( $categoryID ),
+                ),
+                'post_status' => 'publish',
+                'post_author' => 1,
+              );
+              $the_post_id = wp_insert_post( $question, $wp_error );
+              update_field( 'field_5eb02790f5dc1', 'Text Options', $the_post_id ); //type_of_anser
+              update_field( 'field_5ecd5537b4bdc', $data[9] , $the_post_id ); //weight_ansewer
+              update_field( 'field_5eb02ac918c41', 'active', $the_post_id ); //active_question
+              for ($c=0; $c < 5; $c++) {
+                $correctAnswer = [];
+                if( $data[8] == ( $c + 1 ) ) 
+                  $correctAnswer = array( "correct" );
+                add_row( 'field_5eb028ccf5dc5' , array(
+                  'field_5eb028e1f5dc6' => $data[ $c + 3 ], 
+                  'field_5eb02909f5dc7' => $correctAnswer,
+                ), $the_post_id); //options;
+              }
             }
           }
           $row++;
@@ -51,6 +53,60 @@ function enem_simulator_import_questions_callback() {
 }
 
 add_action( 'wp_ajax_enem_simulator_import_questions', 'enem_simulator_import_questions_callback' );
+
+function enem_simulator_import_settings_callback() {
+  $categories = [ [1,2], [3,4], [5,6], [7,8], [9,10] ];
+  if( isset( $_FILES ))
+    $file = $_FILES[ 'file' ][ 'tmp_name' ];
+  $row = 0;
+  try {
+    if( $file ) {
+      if( ( $handle = fopen( $file, 'r' )) !== FALSE ) {
+        while( ( $data = fgetcsv( $handle, 0, ";" )) !== FALSE ) {
+          if( $row !== 0 ) {
+            $num = count( $data );
+            add_row( 'field_5ec6ba18308e9', array(
+              'field_5ec6bc1a99efe' => $data[0], //settings name
+              'field_5eb29ac0e6678' => $data[11], // max time
+              'field_5eb29b71d9526' => $data[12], //end test time alert
+              'field_5eb29c298ef2a' => $data[13],  //end teste activated
+              'field_5eb29cb28ef2b' => $data[14], //activated change questions alert 
+              'field_5ecea8c39cfe9' => $data[15], //weight proficiency
+              'field_5eb29dff8ef2c' => $data[16], //initial message
+            ), 'option'); //settings;
+            if( have_rows('field_5ec6ba18308e9', 'option') ):
+              while ( have_rows('field_5ec6ba18308e9', 'option') ) : the_row();
+                  $name = get_sub_field('field_5ec6bc1a99efe');
+                  if ( $name ===  $data[0] ) {
+                    for ($i=0; $i < 5; $i++) { 
+                      $categoryID = enem_simulator_get_question_category_ID( $data[$categories[$i][0]] );
+                      if ( $categoryID ) {
+                        update_sub_row( 'field_5eb1d416462f6', $i+1, array(
+                          'field_5eb1d53667fbf' => $categoryID, //category
+                          'field_5eb1d4c267fbe' => $data[$categories[$i][1]], //amount
+                          ) );
+                      }
+                    }                      
+                  }
+              endwhile;
+            endif;
+          }
+          $row++;
+        }
+      }
+    }
+  } catch (\Throwable $th) {
+    wp_send_json( array(
+      'message' => $th->getMessage
+    ));
+  }
+  $return = array(
+    'message' => __('Settings imported successfuly!', 'enem-simulator' )
+  );
+  wp_send_json( $return );
+}
+
+add_action( 'wp_ajax_enem_simulator_import_settings', 'enem_simulator_import_settings_callback' );
 
 function enem_simulator_get_question_category_ID($name) {
   global $wpdb;
--- includes/assets/js/enem-simulator-admin.js
+++ includes/assets/js/enem-simulator-admin.js
@@ -7,33 +7,55 @@
     let $submit = $('#submitdiv');
     $submit.parent().remove();
     
-    let $exportGroup = $('.acf-field-5ed1099030710');
-    let $importGroup = $('.acf-field-5ed0f0e5631f1');
+    let $exportQuestionsGroup = $('.acf-field-5ed1099030710');
+    let $exportSettingsGroup = $('.acf-field-5ed55188efb44');
+    let $importQuestionsGroup = $('.acf-field-5ed0f0e5631f1');
+    let $importSettingsGroup = $('.acf-field-5ed5515fefb42');
     
-    let exportContainer = "<div class='acf-label'>"+
-                            "<label for='export-questions-enem-simulator'>Exportar</label>"+
-                            "<p class='description'>Clique em exportar para salvar as questões em um arquivo csv.</p>"+
-                          "</div>"+
-                          "<div class='acf-actions'>"+
-                            "<button type='button' class='button button-primary button-large' id='export-questions-enem-simulator'>Exportar Questões</button>"+
-                          "</div>";
+    let exportQuestionsContainer = "<div class='acf-label'>"+
+                                    "<p class='description'>Clique em exportar para salvar as questões em um arquivo csv.</p>"+
+                                  "</div>"+
+                                  "<div class='acf-actions'>"+
+                                    "<button type='button' class='button button-primary button-large' id='export-questions-enem-simulator'>Exportar Questões</button>"+
+                                  "</div>";
 
-    let importContainer = "<div class='acf-label'>"+
-                            "<label for='import-questions-enem-simulator'>Importar</label>"+
-                            "<p class='description'>Selecione um arquivo no formato csv para realizar a importação das questões.</p>"+
-                          "</div>"+
-                          "<div class='acf-input'>"+
-                            "<label for='upload' id='upload-questions-label-enem-simulator'><span>Nenhum arquivo selecionado </span>" +
-                              "<a href='#' class='acf-button button' id='import-questions-enem-simulator'>Adicionar Arquivo</a>"+
-                              "<input type='file' accept='.csv' id='upload-questions-enem-simulator' style='display:none'>"+
-                            "</label>" +
-                          "</div>"+
-                          "<div class='acf-actions'>"+
-                            "<button type='button' class='button button-primary button-large' id='import-questions-button-enem-simulator'>Importar Questões</button"+
-                          "</div>";
+    let exportSettingsContainer = "<div class='acf-label'>"+
+                                    "<p class='description'>Clique em exportar para salvar as configurações em um arquivo csv.</p>"+
+                                  "</div>"+
+                                  "<div class='acf-actions'>"+
+                                    "<button type='button' class='button button-primary button-large' id='export-settings-enem-simulator'>Exportar Configurações</button>"+
+                                  "</div>";
 
-    $exportGroup.html(exportContainer); 
-    $importGroup.html(importContainer); 
+    let importQuestionsContainer = "<div class='acf-label'>"+
+                                      "<p class='description'>Selecione um arquivo no formato csv para realizar a importação das questões.</p>"+
+                                  "</div>"+
+                                  "<div class='acf-input'>"+
+                                    "<label for='upload' id='upload-questions-label-enem-simulator'><span>Nenhum arquivo selecionado </span>"+
+                                      "<a href='#' class='acf-button button' id='import-questions-enem-simulator'>Adicionar Arquivo</a>"+
+                                      "<input type='file' accept='.csv' id='upload-questions-enem-simulator' style='display:none'>"+
+                                    "</label>"+
+                                  "</div>"+
+                                  "<div class='acf-actions'>"+
+                                    "<button type='button' class='button button-primary button-large' id='import-questions-button-enem-simulator'>Importar Questões</button"+
+                                  "</div>";
+
+    let importSettignsContainer = "<div class='acf-label'>"+
+                                    "<p class='description'>Selecione um arquivo no formato csv para realizar a importação das configurações.</p>"+
+                                  "</div>"+
+                                  "<div class='acf-input'>"+
+                                    "<label for='upload' id='upload-settings-label-enem-simulator'><span>Nenhum arquivo selecionado </span>"+
+                                      "<a href='#' class='acf-button button' id='import-settings-enem-simulator'>Adicionar Arquivo</a>"+
+                                      "<input type='file' accept='.csv' id='upload-settings-enem-simulator' style='display:none'>"+
+                                    "</label>"+
+                                  "</div>"+
+                                  "<div class='acf-actions'>"+
+                                    "<button type='button' class='button button-primary button-large' id='import-settings-button-enem-simulator'>Importar Configurações</button"+
+                                  "</div>";
+
+    $exportQuestionsGroup.html(exportQuestionsContainer); 
+    $exportSettingsGroup.html(exportSettingsContainer); 
+    $importQuestionsGroup.html(importQuestionsContainer); 
+    $importSettingsGroup.html(importSettignsContainer); 
 
     $('#upload-questions-enem-simulator').change(function() {
       let file = $(this)[0].files[0];
@@ -41,8 +63,18 @@
         $('#upload-questions-label-enem-simulator span').text(file.name+' ');
     });
 
+    $('#upload-settings-enem-simulator').change(function() {
+      let file = $(this)[0].files[0];
+      if(file)
+        $('#upload-settings-label-enem-simulator span').text(file.name+' ');
+    });
+
     $('#import-questions-enem-simulator').on('click', function() {
       $('#upload-questions-enem-simulator').trigger('click'); 
+    })
+
+    $('#import-settings-enem-simulator').on('click', function() {
+      $('#upload-settings-enem-simulator').trigger('click'); 
     })
 
     $('#import-questions-button-enem-simulator').on('click', function() {
@@ -65,28 +97,58 @@
           let $button = $('#import-questions-button-enem-simulator');
           $button.text('Importar Questões');
           $button.removeAttr('disabled');
-          let element = '<div class="acf-admin-notice notice notice-success is-dismissible">'+
-                          '<p>'+response.message+'</p>'+
-                          '<button type="button" id="my-dismiss-admin-message" class="notice-dismiss"><span class="screen-reader-text">Dispensar este aviso.</span></button>'+
-                        '</div>';
-          $(element).insertAfter('.wrap h1');
-          $("#my-dismiss-admin-message").click(function(event) {
-            event.preventDefault();
-            $('.notice-success').fadeTo(100, 0, function() {
-                $('.notice-success').slideUp(100, function() {
-                    $('.notice-success').remove();
-                });
-            });
-        });
+          adminNotices('success', response.message);
         },
-        error: function(response) {
-          let element = '<div class="acf-admin-notice notice notice-error is-dismissible">'+
-                          '<p>Ocorreu um erro ao realizar a importação. Verifique o arquivo de importação e tente novamente</p>'+
-                          '<button type="button" class="notice-dismiss"><span class="screen-reader-text">Dispensar este aviso.</span></button>'+
-                        '</div>';
-          $('.wrap').prepend(element);
+        error: function() {
+          adminNotices('error', 'Ocorreu um erro ao realizar a importação. Verifique o arquivo de importação e tente novamente');
         }
       });
     });
+
+    $('#import-settings-button-enem-simulator').on('click', function() {
+
+      $(this).text('Carregando...');
+      $(this).attr('disabled', 'disabled');
+      
+      let fd = new FormData();
+      let file = $('#upload-settings-enem-simulator');
+      let individual_file = file[0].files[0];
+      fd.append('file', individual_file);
+      fd.append('action', 'enem_simulator_import_settings');
+      $.ajax({
+        type: 'POST',
+        url:enem_simulator.ajaxurl,
+        data: fd,
+        contentType: false,
+        processData: false,
+        success: function(response) {
+          let $button = $('#import-settings-button-enem-simulator');
+          $button.text('Importar Questões');
+          $button.removeAttr('disabled');
+          adminNotices('success', response.message);
+        },
+        error: function(response) {
+          adminNotices('error', 'Ocorreu um erro ao realizar a importação. Verifique o arquivo de importação e tente novamente');
+        }
+      });
+    });
+
+    function adminNotices(type, message) {
+      let element = '<div class="notice notice-'+type+' is-dismissible">'+
+                      '<p>'+message+'</p>'+
+                      '<button type="button" id="my-dismiss-admin-message" class="notice-dismiss">'+
+                        '<span class="screen-reader-text">Dispensar este aviso.</span>'+
+                      '</button>'+
+                    '</div>';
+      $(element).insertAfter('.wrap h1');
+      $("#my-dismiss-admin-message").click(function(event) {
+        event.preventDefault();
+        $('.notice-'+type).fadeTo(100, 0, function() {
+            $('.notice-'+type).slideUp(100, function() {
+                $('.notice-'+type).remove();
+            });
+        });
+      });
+    }
   }
 })(jQuery);
