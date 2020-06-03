fbb9cd538439a00a87ebf4b491b8ab6703052831
walfrido-oliveira <walfrido_15@hotmail.com>
2020-06-03 13:54:25 -0300

    fixed bug for equals options list

--- includes/partials/content-question.php
+++ includes/partials/content-question.php
@@ -3,7 +3,7 @@
 <div class="question" style="display:none" id="<?php echo get_the_ID(); ?>">
   <input type="hidden" name="weight_<?php echo get_the_ID(); ?>" id="weight_<?php echo get_the_ID() ?>" value="<?php echo get_field( 'weight_answer', get_the_ID() ) ?>">
   <h3 class="question-category"><?php echo $categoryName; ?></h4>
-	<h4 class="question-title"><?php echo __( 'Question', 'enem-simulator' ); echo (' '); echo $index+1; ?></h4>
+	<h4 class="question-title"><?php echo __( 'Question', 'enem-simulator' ) . ' ' . $questionCount; ?></h4>
   <p class="question-message"> <?php echo $post->post_content ?> </p>
   <div class="figure question-thumbnail">
     <?php the_post_thumbnail( 'post-thumbnail', 
@@ -17,10 +17,10 @@
   <div class="question-options" data-question-index="<?php echo $index ?>">
     <?php 
       if( $fields ): 
-        $name = substr( md5( serialize( $fields ) ), 0, 8 ); 
+        $name = substr( md5( serialize( $fields ) ), 0, 8 ) . rand(); 
         $field = 0;
       ?>
-      <?php foreach( $fields as $value ): $id = substr( md5( serialize( $value ) ), 0, 8 ); $field++;?>
+      <?php foreach( $fields as $value ): $id = substr( md5( serialize( $value ) ), 0, 8 ) . rand(); $field++;?>
         <div class="custom-control custom-checkbox">
           <input type="hidden" name="answer_<?php echo $id ?>" id="answer_<?php echo $id ?>" value="<?php echo $value['correct_answer'][0] ?>">
           <input type="checkbox" class="custom-control-input" id="<?php echo $id ?>" name="<?php echo $name ?>" value="<?php echo $field ?>">
